c6166caa9f64a321e1b94eaaef58048c67480aba
richardlzluo <richard.lz.luo@gmail.com>
2025-05-03 10:53:07 -0400

    minor change

--- app.py
+++ app.py
@@ -287,4 +287,4 @@ def download_report():
         return "Error generating PDF.", 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5018)))
